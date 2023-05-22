d25392aa22071931b8e1c4699262eccb562f0c64
pedromcsousa <pedrosousa1@sapo.pt>
2023-05-22 18:56:27 +0100

    API: Configuração Mongo + Criação schema User

--- api/gateway/src/app.module.ts
+++ api/gateway/src/app.module.ts
@@ -1,12 +1,19 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ClientsModule, Transport } from '@nestjs/microservices';
 import { LorawanModule } from './lorawan/lorawan.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
+import { UserModule } from './user/user.module';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    MongooseModule.forRoot(process.env.MONGODB),
     LorawanModule,
+    UserModule,
   ],
   controllers: [AppController],
   providers: [AppService],
