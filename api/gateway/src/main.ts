d59425aef9e2403319bb00f9012cf63854f81e74
pedromcsousa <pedrosousa1@sapo.pt>
2023-07-18 16:04:23 +0100

    GATEWAY: Inicialização

--- api/gateway/src/main.ts
+++ api/gateway/src/main.ts
@@ -1,16 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, {
-    cors: {
-      methods: 'GET, PUT, POST, DELETE, PATCH',
-      origin: ['*'],
-    },
-  });
-  app.enableCors();
-  app.useGlobalPipes(new ValidationPipe());
-  await app.listen(process.env.PORT);
+  const app = await NestFactory.create(AppModule);
+  await app.listen(3000);
 }
 bootstrap();
